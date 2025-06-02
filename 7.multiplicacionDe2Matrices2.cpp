348f33e5ae0f24db8dfe38efd34f30efcd3d2844
DanielFloresFco <362313052@upapnl.edu.mx>
2025-06-02 10:30:25 -0600

    Update 7.multiplicacionDe2Matrices2.cpp

--- 7.multiplicacionDe2Matrices2.cpp
+++ 7.multiplicacionDe2Matrices2.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     int n;
 
-    // Pedir al usuario el tamaño de la matriz cuadrada
+   
     cout << "Ingrese la dimension de las matrices cuadradas (n x n): ";
     cin >> n;
 
-    // Declarar las matrices dinámicamente con tamaño n x n
+   
     int A[n][n], B[n][n], C[n][n];
 
-    // Ingresar matriz A
+    
     cout << "\nIngrese los elementos de la matriz A:\n";
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j) {
@@ -19,7 +19,7 @@ int main() {
             cin >> A[i][j];
         }
 
-    // Ingresar matriz B
+    
     cout << "\nIngrese los elementos de la matriz B:\n";
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j) {
@@ -27,15 +27,15 @@ int main() {
             cin >> B[i][j];
         }
 
-    // Multiplicar matrices A y B, almacenar en C
+    
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j) {
-            C[i][j] = 0;  // Inicializa elemento
+            C[i][j] = 0;  
             for (int k = 0; k < n; ++k)
                 C[i][j] += A[i][k] * B[k][j];
         }
 
-    // Mostrar resultado
+    
     cout << "\nResultado de la multiplicacion (Matriz C = A x B):\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j)
